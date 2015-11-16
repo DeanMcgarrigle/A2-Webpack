a34937688e98c21217c6eec095f9187c62cebfff
Callum Vass <callum@abaresearch.co.uk>
2015-11-16 11:38:48 +0000

    Remove js and map files

--- src/app/route.config.js
+++
@@ -1,9 +0,0 @@
-var router_1 = require('angular2/router');
-var home_1 = require('./components/home');
-exports.Routes = {
-    home: new router_1.Route({ path: '/', component: home_1.HomeComponent })
-};
-exports.APP_ROUTES = [
-    exports.Routes.home
-];
-//# sourceMappingURL=route.config.js.map
